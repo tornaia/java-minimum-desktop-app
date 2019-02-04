5267ad4678abf701f08f8c9fd2b4fdf558da9877
Andras Tornai <tornaia@users.noreply.github.com>
2019-02-04 20:40:27 +0100

    Update build.jdk13.bat

--- build.jdk13.bat
+++ build.jdk13.bat
@@ -3,6 +3,4 @@ del *.class,*.jar,*.exe
 
 "c:\Program Files\Java\jdk-13\bin\javac" FooClient.java
 "c:\Program Files\Java\jdk-13\bin\jar" cfe fooclient.jar FooClient FooClient.class
-"c:\Program Files\Java\jdk-13\bin\jpackage" -deploy -native installer -outdir . -srcdir . -srcfiles fooclient.jar -appclass FooClient -verbose
-
 "c:\Program Files\Java\jdk-13\bin\jpackage" create-installer exe --input . --files fooclient.jar --main-jar fooclient.jar --class FooClient --output . --verbose
