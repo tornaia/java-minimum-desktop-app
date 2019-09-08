906952e19eaed127c0b42512fbe4463c41bd2c61
Tornai András <tornai.andras.ede@gmail.com>
2019-09-08 13:02:07 +0200

    Cleanup

--- build.jdk13.bat
+++ build.jdk13.bat
@@ -1,6 +1,6 @@
-SET JAVA_HOME=c:\Program Files\Java\jdk-13\
+SET JAVA_HOME=c:\Program Files\Java\jdk-13
 del *.class,*.jar,*.exe
 
-"c:\Program Files\Java\jdk-13\bin\javac" FooClient.java
-"c:\Program Files\Java\jdk-13\bin\jar" cfe fooclient.jar FooClient FooClient.class
-"c:\Program Files\Java\jdk-13\bin\jpackage" create-installer exe --input . --files fooclient.jar --main-jar fooclient.jar --class FooClient --output . --verbose
+"%JAVA_HOME%\bin\javac" FooClient.java
+"%JAVA_HOME%\bin\jar" cfe fooclient.jar FooClient FooClient.class
+"%JAVA_HOME%\bin\jpackage" create-installer exe --input . --files fooclient.jar --main-jar fooclient.jar --class FooClient --output . --verbose
--- build.jdk9.or.jdk10.bat
+++ build.jdk9.or.jdk10.bat
@@ -1,6 +1,6 @@
-SET JAVA_HOME=c:\Program Files\Java\jdk-10.0.2\
+SET JAVA_HOME=c:\Program Files\Java\jdk-10.0.2
 del *.class,*.jar,*.exe
 
-"c:\Program Files\Java\jdk-10.0.2\bin\javac" FooClient.java
-"c:\Program Files\Java\jdk-10.0.2\bin\jar" cfe fooclient.jar FooClient FooClient.class
-"c:\Program Files\Java\jdk-10.0.2\bin\javapackager" -deploy -native installer -outdir . -srcdir . -srcfiles fooclient.jar -appclass FooClient -verbose
+"%JAVA_HOME%\bin\javac" FooClient.java
+"%JAVA_HOME%\bin\jar" cfe fooclient.jar FooClient FooClient.class
+"%JAVA_HOME%\bin\javapackager" -deploy -native installer -outdir . -srcdir . -srcfiles fooclient.jar -appclass FooClient -verbose
